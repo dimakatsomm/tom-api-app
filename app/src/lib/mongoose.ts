813ccc670896906c9acdf31850f49ce8f4d123fc
Dimakatso <dimakatsomm@icloud.com>
2024-08-27 07:58:42 +0200

    cache mongoose connection

--- app/src/lib/mongoose.ts
+++ app/src/lib/mongoose.ts
@@ -1,10 +1,32 @@
 import mongoose from "mongoose";
 
-import * as C from '../constants';
+import * as C from '@/constants';
 
 let cached = (global as any).mongoose;
 
-mongoose.connect(C.MONGODB_URI).then(() => {
-    console.log('Connected to DB.');
-});
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null
+  }
+}
+
+async function connectDb() {
+  if (cached.conn) {
+    return cached.conn
+  }
+
+  if (!cached.promise) {
+    const options = {
+      bufferCommands: false,
+    }
+
+    cached.promise = mongoose.connect(C.MONGODB_URI).then((mongoose) => {
+      console.log('Connected to DB.');
+      return mongoose;
+  });
+  }
+}
+
+export default connectDb;
   
